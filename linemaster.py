3cf6aa3e8a21cfb92da9e62305bc039facc1c15e
Lauri Karanko <lauri.karanko@gmail.com>
2019-10-12 22:09:42 +0300

    chanegd speed values for movement methods

--- linemaster.py
+++ linemaster.py
@@ -6,6 +6,7 @@ from ev3dev2.sensor.lego import TouchSensor
 from ev3dev2.sensor.lego import ColorSensor
 from ev3dev2.sensor.lego import InfraredSensor
 import time
+import math
 
 class LineMaster:
     def __init__(self):
@@ -18,23 +19,42 @@ class LineMaster:
         self.irs = InfraredSensor()
 
     def forward(self, speed, time):
+#        while (self.go_forward):
         self.both_motors.on_for_seconds(SpeedPercent(speed), SpeedPercent(speed), time) # speed 0-100
-#        ground = self.cs.value()
-#        if (ground > 20 or ground < 10) :
-#            self.forward(self, speed, time*1.2)
+            # speed = math.floor(speed*1.2) : 100
+            # time = time*1.2
 
     def turn (self, speed, time):
         ground = self.cs.value()
         if ground > 20:
             while (self.cs.value() > 10):
                 print(self.cs.value)
+                # speed = math.floor(speed*1.2) : 100
+                # time = time*1.2
                 self.both_motors.on_for_seconds(SpeedPercent(speed), SpeedPercent(speed/6), time) # speed 0-100
-                # self.left_motor.on_for_seconds(SpeedPercent(speed), time)
         if ground < 10:
             while (self.cs.value() < 20):
                 print(self.cs.value)
+                # speed = math.floor(speed*1.2) : 100
+                # time = time*1.2
                 self.both_motors.on_for_seconds(SpeedPercent(speed/6), SpeedPercent(speed), time) # speed 0-100
                 # self.right_motor.on_for_seconds(SpeedPercent(speed), time)
+
+#    def go_left(self)
+#        if self.cs.value() > 20:
+#            return True
+#        return False
+#
+#    def go_right()
+#        if self.cs.value() > 10:
+#            return True
+#        return False
+#
+#    def go_forward():
+#        ground = self.cs.value()
+#        if (ground <= 20 and ground >= 10):
+#            return True
+#        return False
 
     def run(self):
         # mode and asserts
@@ -43,10 +63,9 @@ class LineMaster:
         # self.irs.mode = 'US-DIST-CM' # distance
 
         while not self.shut_down:
-            self.turn(25, 0.1)
+            self.turn(50, 0.1)
             self.forward(50, 0.1)
             print(self.cs.value())
-            # time.sleep(0.1)
 
 # Main function
 if __name__ == "__main__":
