6b6643d25f9471318f92ed46d38c372c7758733c
Lauri Karanko <lauri.karanko@gmail.com>
2019-10-12 19:50:25 +0300

    it works

--- linemaster.py
+++ linemaster.py
@@ -1,40 +1,44 @@
 #!/usr/bin/env python3
 import ev3dev.ev3 as ev3
-#from ev3dev2.motor import MediumMotor, LargeMotor, OUTPUT_A, OUTPUT_B, OUTPUT_C, SpeedPercent, MoveTank
-#from ev3dev2.sensor import INPUT_1
-#from ev3dev2.sensor.lego import TouchSensor
-#from ev3dev2.sensor.lego import ColorSensor
-#from ev3dev2.sensor.lego import InfraredSensor
-from time import sleep
+from ev3dev2.motor import MediumMotor, LargeMotor, OUTPUT_A, OUTPUT_B, OUTPUT_C, SpeedPercent, MoveTank
+from ev3dev2.sensor import INPUT_1
+from ev3dev2.sensor.lego import TouchSensor
+from ev3dev2.sensor.lego import ColorSensor
+from ev3dev2.sensor.lego import InfraredSensor
+import time
 
 class LineMaster:
     def __init__(self):
         self.shut_down = False
         # self.claw_motor
-        self.left_motor = ev3.LargeMotor('outB')
-        self.right_motor = ev3.LargeMotor('outC')
-        # self.both_motors = ev3.MoveTank('outB', 'outC')
-        self.cs = ev3.ColorSensor()
-        self.irs = ev3.InfraredSensor()
+        self.left_motor = LargeMotor('outB')
+        self.right_motor = LargeMotor('outC')
+        self.both_motors = MoveTank('outB', 'outC')
+        self.cs = ColorSensor()
+        self.irs = InfraredSensor()
 
     def forward(self, speed, time):
-        self.left_motor.on_for_rotations(SpeedPercent(speed), time)
-        self.right_motor.on_for_rotations(SpeedPercent(speed), time)
-        # self.both_motors.on_for_rotations(SpeedPercent(speed), SpeedPercent(speed), time) # speed 0-100
+        #self.left_motor.on_for_seconds(SpeedPercent(speed), time)
+        #self.right_motor.on_for_seconds(SpeedPercent(speed), time)
+        self.both_motors.on_for_seconds(SpeedPercent(speed), SpeedPercent(speed), time) # speed 0-100
 
     def turn (self, direction, speed, time):
         if direction == "left":
-            while (cs.value() > 5):
-                self.left_motor.on_for_rotations(SpeedPercent(speed), time)
+            while (self.cs.value() > 10):
+                print(self.cs.value)
+                self.both_motors.on_for_seconds(SpeedPercent(speed), SpeedPercent(speed/2), time) # speed 0-100
+                # self.left_motor.on_for_seconds(SpeedPercent(speed), time)
         if direction == "right":
-            while (cs.value() < 15):
-                self.right_motor.on_for_rotations(SpeedPercent(speed), time)
+            while (self.cs.value() < 20):
+                print(self.cs.value)
+                self.both_motors.on_for_seconds(SpeedPercent(speed/2), SpeedPercent(speed), time) # speed 0-100
+                # self.right_motor.on_for_seconds(SpeedPercent(speed), time)
 
     def turnDirection(self):
-        ground = cs.value()
-        if ground > 15 :
+        ground = self.cs.value()
+        if ground > 20 :
             return "left"
-        elif ground < 5 :
+        elif ground < 10 :
             return "right"
         else:
             return "false"
@@ -44,16 +48,12 @@ class LineMaster:
         # mode and asserts
         self.cs.mode = 'COL-REFLECT'  # measure light intensity
         # self.irs.mode = 'US-DIST-CM' # distance
-        # claw; assert self.claw.connected  # medium motor
-        # self.cs; assert self.cs.connected
-        self.right_motor; assert self.right_motor.connected
-        self.left_motor; assert self.left_motor.connected
 
         while not self.shut_down:
-            turn(turnDirection(), 25, 0.1)
-            forward(50, 0.1)
-            print(cs.value())
-            time.sleep(0.1)
+            self.turn(self.turnDirection(), 25, 0.1)
+            self.forward(50, 0.1)
+            print(self.cs.value())
+            # time.sleep(0.1)
 
 # Main function
 if __name__ == "__main__":
