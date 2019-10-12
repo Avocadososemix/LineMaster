40e8fef6cf2d516d1246b3439dd3832377d04f0d
Lauri Karanko <lauri.karanko@gmail.com>
2019-10-12 17:59:57 +0300

    minor change in imports

--- linemaster.py
+++ linemaster.py
@@ -1,23 +1,26 @@
 #!/usr/bin/env python3
-from ev3dev2.motor import MediumMotor, LargeMotor, OUTPUT_A, OUTPUT_B, OUTPUT_C, SpeedPercent, MoveTank
-from ev3dev2.sensor import INPUT_1
-from ev3dev2.sensor.lego import TouchSensor
-from ev3dev2.sensor.lego import ColorSensor
-from ev3dev2.sensor.lego import InfraredSensor
+import ev3dev.ev3 as ev3
+#from ev3dev2.motor import MediumMotor, LargeMotor, OUTPUT_A, OUTPUT_B, OUTPUT_C, SpeedPercent, MoveTank
+#from ev3dev2.sensor import INPUT_1
+#from ev3dev2.sensor.lego import TouchSensor
+#from ev3dev2.sensor.lego import ColorSensor
+#from ev3dev2.sensor.lego import InfraredSensor
 from time import sleep
 
 class LineMaster:
     def __init__(self):
         self.shut_down = False
         # self.claw_motor
-        self.left_motor = LargeMotor(OUTPUT_B)
-        self.right_motor = LargeMotor(OUTPUT_C)
-        self.both_motors = MoveTank(OUTPUT_B, OUTPUT_C)
-        self.cs = ColorSensor()
-        self.irs = InfraredSensor()
+        self.left_motor = ev3.LargeMotor('outB')
+        self.right_motor = ev3.LargeMotor('outC')
+        # self.both_motors = ev3.MoveTank('outB', 'outC')
+        self.cs = ev3.ColorSensor()
+        self.irs = ev3.InfraredSensor()
 
     def forward(self, speed, time):
-        self.both_motors.on_for_rotations(SpeedPercent(speed), SpeedPercent(speed), time) # speed 0-100
+        self.left_motor.on_for_rotations(SpeedPercent(speed), time)
+        self.right_motor.on_for_rotations(SpeedPercent(speed), time)
+        # self.both_motors.on_for_rotations(SpeedPercent(speed), SpeedPercent(speed), time) # speed 0-100
 
     def turn (self, direction, speed, time):
         if direction == "left":
@@ -47,8 +50,8 @@ class LineMaster:
         self.left_motor; assert self.left_motor.connected
 
         while not self.shut_down:
-            forward(50, 0.1)
             turn(turnDirection(), 25, 0.1)
+            forward(50, 0.1)
             print(cs.value())
             time.sleep(0.1)
 
